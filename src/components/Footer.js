839afa5c32f1cc5f334b34bbc77b014b3ce0fb43
Junyeong0218 <hippo2003@naver.com>
2023-12-21 18:13:00 +0900

    modify coupons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,23 +1,8 @@
 import React, { useState } from "react";
+import FooterCoupon from "./footerCoupon";
 
 export default function Footer() {
-    const [complete, setComplete] = useState("");
-    const [copyText, setCopyText] = useState("복사하기");
-    const coupon = "DSR1THX";
-
-    const copyToClipboard = () => {
-        navigator.clipboard.writeText(coupon).then(() => {
-            setComplete("complete");
-            setCopyText("복사완료!");
-
-            setTimeout(() => {
-                setComplete("");
-                setCopyText("복사하기");
-            }, 2500);
-        }).catch(error => {
-            alert("복사에 실패했습니다.\n관리자에게 문의해주세요.");
-        });
-    }
+    const coupons = ["2023XMAS", "ADIEU2023"];
 
     return (
         <div className="footer">
@@ -25,10 +10,9 @@ export default function Footer() {
                 <span className="description">개인이 만든 홈페이지로 게임사와 무관합니다.</span>
                 <span className="description">오류, 개선 등 문의는 카페 댓글 혹은 <img src="/images/discord.png"/> 디스코드 hippo2003으로 연락바랍니다.</span>
             </div>
-            <div className="event">
-                <span className="title">1주년 쿠폰</span>
-                <span className="description accent">쿠폰번호 : {coupon}</span>
-                <button type="button" className={`copy-button ${complete}`} onClick={copyToClipboard}>{copyText}</button>
+            <div className="events">
+                <FooterCoupon title="크리스마스 쿠폰" coupon={coupons[0]}/>
+                <FooterCoupon title="연말 쿠폰" coupon={coupons[1]}/>
             </div>
         </div>
     );
