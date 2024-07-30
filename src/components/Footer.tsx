39ab398f44c15f5b0ce282d0d0487ccf562f44ea
Junyeong0218 <hippo2003@naver.com>
2024-07-31 02:33:44 +0900

    remove footer link

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,7 +7,6 @@ export default function Footer(): React.ReactElement {
         <div className="footer">
             <div className="descriptions">
                 <span className="description">개인이 만든 홈페이지로 게임사와 무관합니다.</span>
-                <span className="description">오류, 개선 등 문의는 <a href="https://discord.gg/SmfhwyqMYZ" target="_blank"><img src={`${IMG_URL_BASE}/discord.png`}/>위키 피드백 디스코드 서버 <i className="fa-solid fa-arrow-up-right-from-square"></i></a> 로 연락바랍니다.</span>
             </div>
         </div>
     );
